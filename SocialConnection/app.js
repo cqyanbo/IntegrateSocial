c85357a830c9e74e25be620796113ab051eca261
cqyanbo <cqyanbo@gmail.com>
2015-04-09 10:25:49 -0500

    add third party libraries

--- SocialConnection/app.js
+++ SocialConnection/app.js
@@ -1,11 +1,5 @@
 var express = require('express');
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune/fortune.js');
 var app = express();
 //set up handlebars view engine
 var handlebars = require('express3-handlebars')
@@ -18,8 +12,7 @@ app.set('view engine', 'handlebars');
 	});
 	
 	app.get('/about', function(req, res) {
-		var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-		res.render('about', { fortune: randomFortune });
+		res.render('about', { fortune: fortune.getFortune() } );
 	});
 	
 	// 404 catch-all handler (middleware)
